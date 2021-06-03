500242b5d479f4001585af97b1672fdcdf6b650c
Daniel Henriques <57723730+danielhrqs4@users.noreply.github.com>
2021-06-03 20:38:00 -0300

    .

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,9 @@
-alert("Hello World")
+let t1 = document.getElementById("t1");
+let t2 = document.getElementById("t2");
+
+t1.onclick = deixaMaiusculo;
+t2.onclick = deixaMaiusculo;
+
+function deixaMaiusculo(){
+    this.innerHTML = this .innerHTML.toUpperCase();
+}
